06f19ea45603fffc6ad21e289de7d6652e10444b
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-11-23 17:33:03 +0100

    avancement

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import HomePage from './HomePage'
+import Nav from './Nav';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: darkblue;
+    }
+`
 
+const Row = styled.div`
+    display: flex;
+    flex-direction: row; 
+    justify-content: space-between
+`
+
+const Justified = styled.div`
+    display: flex; 
+    flex-direction : column;
+    padding-right: 40px;
+`
+
+const Titles = styled.h3`
+    margin: 3px 0
+`
+
+const HostelPicture = styled.img`
+height : 300px;
+width : 300px;
+`
 const Hotel = props => {
 
     const [hotel, setHotel] = useState([])
@@ -16,22 +42,56 @@ const Hotel = props => {
     console.log(hotel.map(e => e));
     return (
         <>
-            <HomePage />
-            <div>
-                <img src="https://www.parisinfo.com/var/otcp/sites/images/media/1.-photos/03.-hebergement-630-x-405/hotel-enseigne-neon-630x405-c-thinkstock/31513-1-fre-FR/Hotel-enseigne-neon-630x405-C-Thinkstock.jpg" alt="Hotel" />
-            </div>
-            <div>
-                <h1>Hôtel {props.name}</h1>
-            </div>
-            <div>
-                
-                {hotel.map(hostel => (
-                    <p>{hostel.rooms}</p>
-                ))}
-
-            </div>
+            <GlobalStyle />
+            <Nav />
+            <Row>
+                <Justified>
+                    <HostelPicture src="https://www.parisinfo.com/var/otcp/sites/images/media/1.-photos/03.-hebergement-630-x-405/hotel-enseigne-neon-630x405-c-thinkstock/31513-1-fre-FR/Hotel-enseigne-neon-630x405-C-Thinkstock.jpg" alt="Hotel" />
+                    <h1>Hôtel (name)</h1>
+                    <Titles>Adresse de L'Hôtel (address) + Cité (city)</Titles>
+                    <Titles>Pays (country) </Titles>
+                    <Titles>Téléphone (phone)</Titles>
+                    <Titles>Prix de l'hôtel (price) + Nombre d'étoiles (stars)</Titles>
+                </Justified>
+                <div>
+                    <p> Commoditées (commodities) </p>
+                    <p>"swimming pool",
+                        "restaurant",
+                        "gym",
+                        "room service",
+                        "wifi",
+                        "bar",
+                        "spa",
+                        "disabled access",
+                        "family",
+                        "wifi",
+                        "conciergerie",
+                        "non smoking",
+                        "shuttle",
+                        "dry cleaning",
+                        "multilingual staff",
+                        "breakfast included",
+                        "swimming pool",
+                        "swimming pool",
+                        "swimming pool",
+                        "meeting rooms",
+                        "meeting rooms",
+                        "meeting rooms",
+                        "dry cleaning",
+                        "wifi",
+                        "air conditioning",
+                        "minibar",
+                        "minibar",
+                        "disabled access",
+                        "family",
+                        "non smoking",
+                        "suites"
+                    </p>
+                </div>
+            </Row>
         </>
     );
 };
+
 
 export default Hotel;
