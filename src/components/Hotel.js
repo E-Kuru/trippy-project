5e84e0ffa49e3677e35e2a2ed3b3972dcd819f39
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-11-23 14:29:39 +0100

    Affichage Hotel

---
+++ src/components/Hotel.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+
+const Hotel = props => {
+    return (
+        <>
+            <div>
+                <img src="https://www.parisinfo.com/var/otcp/sites/images/media/1.-photos/03.-hebergement-630-x-405/hotel-enseigne-neon-630x405-c-thinkstock/31513-1-fre-FR/Hotel-enseigne-neon-630x405-C-Thinkstock.jpg" alt="Hotel" />
+            </div>
+        </>
+    );
+};
+
+export default Hotel;
