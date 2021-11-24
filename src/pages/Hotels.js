bf22972bb87c55396316a7092b60261582ed13d7
E-Kuru <evandalleaublop@gmail.com>
2021-11-24 12:38:24 +0100

    Map component Added

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -2,9 +2,13 @@ import {useEffect, useState} from 'react'
 import { useParams } from 'react-router'
 
 import Nav from '../components/Nav'
+import CityMap from '../components/CityMap'
+import Markers from '../components/Markers'
 
 
 const Hotels = () => {
+
+    const [myPosition, setMyPosition] = useState(null)
 
     const [cities, setCities] = useState([])
 
@@ -16,12 +20,32 @@ const Hotels = () => {
         .then(res => setCities(res))
     },[])
 
-    console.log(cities.results);
+    if (!cities.center) {
+        return <p>Chargement...</p>
+    }
+
+    console.log(cities);
 
     return(
         <>
         <Nav/>
+        <CityMap 
+        center={cities.center}
+        >
+        
+        {cities.results.map( e => (
+            <Markers 
+            key={e.name + e.location}
+            color='#094BBC'
+            lat={e.location.lat}
+            lng={e.location.lon}
+            price={e.price}
+            />
+        ))}
+
+        </CityMap>
         <p>{city}</p>
+
         </>
     )
 }
