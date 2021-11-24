8186b4168e76f534a8ab62753f18a4d05985c679
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-11-24 13:54:08 +0100

    en cours

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -8,9 +8,11 @@ import Nav from '../components/Nav'
 const Hotels = () => {
     
     const Card = styled.div`
+    
     font-size: 1em;
       border-radius: 3px;
       color: black ;
+      font-weight: bold;
       margin: 10px;
       margin-bottom: 15px;
       padding: 15px;
@@ -18,44 +20,57 @@ const Hotels = () => {
       width: 300px;
       height: 550px;
       border: 1px solid #c3c3c3;
+      
       `
 
+      const Img = styled.img`
+      height: 250px;
+      
+
+      `
+     
+    
     const [cities, setCities] = useState([])
 
     const {city} = useParams()
 
-    useEffect(() =>{
+    useEffect( () => {
         fetch (`https://trippy-konexio.herokuapp.com/api/hotels/city/${city}`)
         .then(res => res.json())
-        .then(res => setCities(res))
+        .then(res => setCities(res.results))
     },[])
 
-    console.log(cities.results);
-
+    console.log(cities);
+    console.log(city);
     return(
 
         
         <>
+        
         <Nav/>
-        <h1>Hotels List</h1>
+        <h1> Hotels List</h1>
+        
         {cities.map(hotel =>
            
         <Card key={hotel.name}>
 
             {/* <img src={hotel.pictures}/> */}
+            <div className="title">
             <h3>{hotel.name}</h3>
+            </div>
             
             <p>{hotel.stars} ★</p>
             <Img src={'https://www.lippi.fr/wp-content/uploads/2017/11/NoirCarbone.jpg'}/>
             <p>{hotel.address}</p>
             <p>{hotel.phone}</p>
             <p>{hotel.city}</p>
-            <p>{hotel.price} €</p>
+            <p>{hotel.price}€</p>
             
  
        
         </Card>
             )}
+        
             
 
         <p>{city}</p>
